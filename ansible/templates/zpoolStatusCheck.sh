612030a0b3a9d6fefe1f73d16b94b98e2771b7ae
Oliver Hilsky <oliver@hilsky.de>
2022-02-07 13:56:01 +0100

    added zpool status checker script

---
+++ ansible/templates/zpoolStatusCheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# pool to check
+POOL="dataPool"
+
+# needed paths
+LOGFILE="/var/log/poolStatusCheck.log"
+ZFS="/sbin/zfs"
+
+# pushover data
+PO_TOKEN="pushOverTokenFromYourAccount"
+PO_UK="pushOverUserKeyFromYourAccount"
+
+# -------------- program, don't change ---------------
+
+if [ "$(zpool status $POOL -x)" != "pool '$POOL' is healthy" ]; then
+        echo "$(date) - Alarm - zPool $POOL is not healthy" >> $LOGFILE
+        curl -s -F "token=$PO_TOKEN" \
+    -F "user=$PO_UK" \
+    -F "title=Zpool status unhealthy!" \
+    -F "message=The status of pool $POOL does not seem to healthy" https://api.pushover.net/1/message.json
+else
+        echo "$(date) - Zpool $POOL status is healthy" >> $LOGFILE
+fi
